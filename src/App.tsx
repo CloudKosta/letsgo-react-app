77bc616a8d81effa84beb90606edd478b859cdc0
sinee0601 <sinee0601@gmail.com>
2026-06-30 20:35:58 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-transparent flex items-center justify-center font-sans">
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-slate-400 text-sm">내부 UI 작성 공간</p>
+      </div>
+    </div>
+  );
+}
