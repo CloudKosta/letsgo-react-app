7a496607b631075593bf4b3f1da6ce2ddf6804de
Mskko <mskk0410@gmail.com>
2026-07-02 14:25:27 +0900

    chore: Move DropDown, PostScheduleDetail, PostscheduleApp, and TabButton components

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import App from './App.tsx'
 import './index.css'
-import PostScheduleApp from './components/postscheudle/PostScheduleApp'
+import PostScheduleApp from './screens/postSchedule/PostscheduleApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
