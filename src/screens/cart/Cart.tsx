2eec075adb83798f795174e4eb33e39a67cd2825
cloud44112 <cloud44112@gmail.com>
2026-07-06 14:45:46 +0900

    feat: implement Cart screen layout with item listing and scheduling functionality

--- src/screens/cart/Cart.tsx
+++ src/screens/cart/Cart.tsx
@@ -1,4 +1,4 @@
-import CartHeader from "./CartHeader";
+
 import CartCount from "./CartCount";
 import CartItem from "./CartItem";
 import ScheduleButton from "./ScheduleButton";
@@ -10,7 +10,6 @@ export default function Cart() {
 
     return (
         <div className="flex flex-col min-h-full pb-6">
-            <CartHeader />
             <div className="flex-grow px-4 flex flex-col justify-between">
                 <div>
                     <CartCount />
